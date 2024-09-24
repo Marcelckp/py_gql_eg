77cb3b14de0201c4ba0bad1a75701f71a29abeaa
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2024-09-24 22:05:23 +0200

    chore: import querys and mutations to create GQL schema and endpoint for querying data from the db

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import strawberry
 from fastapi import FastAPI
+from core import Mutation, Query
+from strawberry.fastapi import GraphQLRouter
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
+graphql_app = GraphQLRouter(schema=schema)
+
+# Create fast api application
 app = FastAPI()
-@app.get("/")
+
+# This will add the graphql app to the main app as a route /graphql with the following methods ['GET', 'POST']
+# This is an alternative to the @app.get("/graphql") and @app.post("/graphql") decorators respectively
+app.include_router(graphql_app, prefix="/graphql")
+
+@app.get("/health")
+def read_root():
+    return {"Looking": "Good"}
+
+@app.get("/demo")
 def ping():
-    return {"Hello": "World"}
+    return {"Test": "Response"}
