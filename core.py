2b7e109f4b35fcd9d86b83ef86ef87f0103dcdfa
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2024-09-24 22:07:08 +0200

    chore: imported mutations and queries from the controller and created type endpoints for our graphql to use

--- core.py
+++ core.py
@@ -5,7 +5,13 @@ from schema import UserType, PostType, CommentsType
 
 @strawberry.type
 class Mutation:
-    temp : int = None
+    # temp : int = None
+    add_user: UserType = strawberry.mutation(resolver=CreateMutation.add_user)
+    add_post: PostType = strawberry.mutation(resolver=CreateMutation.add_post)
+    add_comment: CommentsType = strawberry.mutation(resolver=CreateMutation.add_comment)
+
 @strawberry.type
 class Query:
-    temp : int = None
+    # temp : int = None
+    list_users: List[UserType] = strawberry.field(resolver=Queries.get_all_users)
+    get_user: UserType = strawberry.field(resolver=Queries.get_user)
